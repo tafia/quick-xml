a91ba5ea2c7f70e7d58e67ed95537d6a97707aea
Johann Tuffe <tafia973@gmail.com>
2016-03-31 19:33:59 +0800

    add benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -53,3 +53,22 @@ fn bench_quick_xml_namespaced(b: &mut Bencher) {
     });
 }
 
+#[bench]
+fn bench_quick_xml_escaped(b: &mut Bencher) {
+    let src: &[u8] = include_bytes!("../tests/sample_rss.xml");
+    b.iter(|| {
+        let r = XmlReader::from_reader(src);
+        let mut count = test::black_box(0);
+        let mut nbtxt = test::black_box(0);
+        for e in r {
+            match e {
+                Ok(Event::Start(_)) => count += 1,
+                Ok(Event::Text(ref e)) => nbtxt += e.unescaped_content().unwrap().len(),
+                _ => (),
+            }
+        }
+        assert!(count == 1550);
+        assert!(nbtxt == 66063);
+    });
+}
+
