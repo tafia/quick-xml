d5c89568edc398892b086dabe4c5a66800d94676
Johann Tuffe <tafia973@gmail.com>
2017-07-06 09:31:23 +0800

    Allow writer to borrow Event, using Deref
    This pull request strictly improves the current situation (shouldn't be a breaking change).  
    
    Fixes situation like https://github.com/tafia/quick-xml/pull/56#issuecomment-310488378

--- src/writer.rs
+++ src/writer.rs
@@ -4,6 +4,7 @@ use std::io::Write;
 
 use errors::Result;
 use events::Event;
+use std::ops::Deref;
 
 /// Xml writer
 ///
@@ -75,8 +76,8 @@ impl<W: Write> Writer<W> {
     }
 
     /// Writes the given event to the underlying writer.
-    pub fn write_event(&mut self, event: Event) -> Result<usize> {
-        match event {
+    pub fn write_event<'a, E: Deref<Event<'a>>(&mut self, event: E) -> Result<usize> {
+        match *event {
             Event::Start(ref e) => self.write_wrapped(b"<", e, b">"),
             Event::End(ref e) => self.write_wrapped(b"</", e, b">"),
             Event::Empty(ref e) => self.write_wrapped(b"<", e, b"/>"),
