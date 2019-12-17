7ea3fecd683433824649100564a36ee0cf4db5c8
Johann Tuffe <tafia973@gmail.com>
2019-12-17 16:16:24 +0800

    update tests

--- tests/serde_roundtrip.rs
+++ tests/serde_roundtrip.rs
@@ -75,3 +75,41 @@ fn round_trip_list_of_enums() {
     let deserialized_nodes: Nodes = from_str(serialized_nodes.as_str()).unwrap();
     assert_eq!(deserialized_nodes, nodes);
 }
+
+#[test]
+fn no_contiguous_fields(){
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    struct Xml {
+        #[serde(rename= "$value")]
+        fields: Vec<Field>,
+    }
+
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    enum Field {
+        #[serde(rename = "field1")]
+        Field1 { name: String },
+        #[serde(rename = "field2")]
+        Field2 { name: String },
+    }
+
+    let source = r#"
+<Xml>
+    <field1 name='a'/>
+    <field2 name='b'/>
+    <field1 name='a'/>
+</Xml>
+"#;
+
+    let xml: Xml = ::quick_xml::de::from_str(source).unwrap();
+    assert_eq!(xml, Xml {
+        fields: vec![
+            Field::Field1 { name: "a".to_string() },
+            Field::Field2 { name: "b".to_string() },
+            Field::Field1 { name: "a".to_string() },
+        ],
+    });
+
+    // TODO: impl Serialize for struct variants
+    // let serialized = to_string(&xml).unwrap();
+    // assert_eq!(serialized, source);
+}
