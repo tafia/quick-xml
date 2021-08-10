e35cbdc2ca38fde2eb76e850f7379644ed6beea0
Johann Tuffe <tafia973@gmail.com>
2021-08-10 21:27:27 +0800

    fix import error

--- src/errors.rs
+++ src/errors.rs
@@ -52,10 +52,10 @@ impl From<::std::str::Utf8Error> for Error {
     }
 }
 
-impl From<::escape::EscapeError> for Error {
+impl From<crate::escape::EscapeError> for Error {
     /// Creates a new `Error::EscapeError` from the given error
     #[inline]
-    fn from(error: ::escape::EscapeError) -> Error {
+    fn from(error: crate::escape::EscapeError) -> Error {
         Error::EscapeError(error)
     }
 }
