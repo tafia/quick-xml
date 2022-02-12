f4fddfbb66e0e2f913c087dc5c4ef45213ee890e
Mingun <Alexander_Sergey@mail.ru>
2022-02-12 20:35:26 +0500

    Move end-of-loop processing outside of loop

--- src/reader.rs
+++ src/reader.rs
@@ -1176,12 +1176,11 @@ impl<'b, 'i, R: BufRead + 'i> BufferedInput<'b, 'i, &'b mut Vec<u8>> for R {
                                     _ => state,
                                 };
                             }
-                            None => {
-                                buf.extend_from_slice(available);
-                                return available.len();
-                            }
+                            None => break,
                         }
                     }
+                    buf.extend_from_slice(available);
+                    return available.len();
                 };
                 find(b'>')
             };
