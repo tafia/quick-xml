4882ed3157a64c9c569a06fb933c1d4270476aa6
Mingun <Alexander_Sergey@mail.ru>
2022-03-08 00:26:53 +0500

    Add canary test for correct comment in `dashes_in_comments` test

--- tests/xmlrs_reader_tests.rs
+++ tests/xmlrs_reader_tests.rs
@@ -194,6 +194,17 @@ fn dashes_in_comments() {
         "#,
         true,
     );
+
+    // Canary test for correct comments
+    test(
+        r#"<!-- comment --><hello/>"#,
+        r#"
+        |Comment( comment )
+        |EmptyElement(hello)
+        |EndDocument
+        "#,
+        true,
+    );
 }
 
 #[test]
