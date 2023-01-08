38929d72113eaaef3ed52f3bf9cf7c1e73cdab25
Mingun <alexander_sergey@mail.ru>
2023-01-08 12:39:01 +0500

    Add regression tests for #252

--- tests/serde-issues.rs
+++ tests/serde-issues.rs
@@ -6,6 +6,78 @@ use quick_xml::de::from_str;
 use quick_xml::se::to_string;
 use serde::{Deserialize, Serialize};
 
+/// Regression tests for https://github.com/tafia/quick-xml/issues/252.
+mod issue252 {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn attributes() {
+        #[derive(Serialize, Debug, PartialEq)]
+        struct OptionalAttributes {
+            #[serde(rename = "@a")]
+            a: Option<&'static str>,
+
+            #[serde(rename = "@b")]
+            #[serde(skip_serializing_if = "Option::is_none")]
+            b: Option<&'static str>,
+        }
+
+        assert_eq!(
+            to_string(&OptionalAttributes { a: None, b: None }).unwrap(),
+            r#"<OptionalAttributes a=""/>"#
+        );
+        assert_eq!(
+            to_string(&OptionalAttributes {
+                a: Some(""),
+                b: Some("")
+            })
+            .unwrap(),
+            r#"<OptionalAttributes a="" b=""/>"#
+        );
+        assert_eq!(
+            to_string(&OptionalAttributes {
+                a: Some("a"),
+                b: Some("b")
+            })
+            .unwrap(),
+            r#"<OptionalAttributes a="a" b="b"/>"#
+        );
+    }
+
+    #[test]
+    fn elements() {
+        #[derive(Serialize, Debug, PartialEq)]
+        struct OptionalElements {
+            a: Option<&'static str>,
+
+            #[serde(skip_serializing_if = "Option::is_none")]
+            b: Option<&'static str>,
+        }
+
+        assert_eq!(
+            to_string(&OptionalElements { a: None, b: None }).unwrap(),
+            r#"<OptionalElements><a/></OptionalElements>"#
+        );
+        assert_eq!(
+            to_string(&OptionalElements {
+                a: Some(""),
+                b: Some("")
+            })
+            .unwrap(),
+            r#"<OptionalElements><a/><b/></OptionalElements>"#
+        );
+        assert_eq!(
+            to_string(&OptionalElements {
+                a: Some("a"),
+                b: Some("b")
+            })
+            .unwrap(),
+            r#"<OptionalElements><a>a</a><b>b</b></OptionalElements>"#
+        );
+    }
+}
+
 /// Regression test for https://github.com/tafia/quick-xml/issues/537.
 ///
 /// This test checks that special `xmlns:xxx` attributes uses full name of
