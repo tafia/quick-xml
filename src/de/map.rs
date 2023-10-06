64f810745827961753b0529262e397532590981c
Mingun <Alexander_Sergey@mail.ru>
2023-10-06 21:20:33 +0500

    Implement EnumAccess directly for SeqItemDeserializer
    This will be needed later when we split SeqItemDeserializer into two deserializers.
    Just copied implementation of crate::de::var::EnumAccess and slightly tune lifetimes

--- src/de/map.rs
+++ src/de/map.rs
@@ -12,7 +12,7 @@ use crate::{
     name::QName,
 };
 use serde::de::value::BorrowedStrDeserializer;
-use serde::de::{self, DeserializeSeed, MapAccess, SeqAccess, Visitor};
+use serde::de::{self, DeserializeSeed, Deserializer as _, MapAccess, SeqAccess, Visitor};
 use serde::serde_if_integer128;
 use std::borrow::Cow;
 use std::ops::Range;
@@ -964,7 +964,7 @@ where
     where
         V: Visitor<'de>,
     {
-        visitor.visit_enum(crate::de::var::EnumAccess::new(self.map.de))
+        visitor.visit_enum(self)
     }
 
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
@@ -974,6 +974,124 @@ where
         match self.map.de.peek()? {
             DeEvent::Text(_) => self.deserialize_str(visitor),
             _ => self.deserialize_map(visitor),
+        }
+    }
+}
+
+impl<'de, 'd, 'm, R, E> de::EnumAccess<'de> for SeqItemDeserializer<'de, 'd, 'm, R, E>
+where
+    R: XmlRead<'de>,
+    E: EntityResolver,
+{
+    type Error = DeError;
+    type Variant = VariantAccess<'de, 'm, R, E>;
+
+    fn variant_seed<V>(self, seed: V) -> Result<(V::Value, Self::Variant), Self::Error>
+    where
+        V: DeserializeSeed<'de>,
+    {
+        let decoder = self.map.de.reader.decoder();
+        let (name, is_text) = match self.map.de.peek()? {
+            DeEvent::Start(e) => (
+                seed.deserialize(QNameDeserializer::from_elem(e.raw_name(), decoder)?)?,
+                false,
+            ),
+            DeEvent::Text(_) => (
+                seed.deserialize(BorrowedStrDeserializer::<DeError>::new(TEXT_KEY))?,
+                true,
+            ),
+            DeEvent::End(e) => return Err(DeError::UnexpectedEnd(e.name().into_inner().to_vec())),
+            DeEvent::Eof => return Err(DeError::UnexpectedEof),
+        };
+        Ok((
+            name,
+            VariantAccess {
+                de: self.map.de,
+                is_text,
+            },
+        ))
+    }
+}
+
+struct VariantAccess<'de, 'd, R, E>
+where
+    R: XmlRead<'de>,
+    E: EntityResolver,
+{
+    de: &'d mut Deserializer<'de, R, E>,
+    /// `true` if variant should be deserialized from a textual content
+    /// and `false` if from tag
+    is_text: bool,
+}
+
+impl<'de, 'd, R, E> de::VariantAccess<'de> for VariantAccess<'de, 'd, R, E>
+where
+    R: XmlRead<'de>,
+    E: EntityResolver,
+{
+    type Error = DeError;
+
+    fn unit_variant(self) -> Result<(), Self::Error> {
+        match self.de.next()? {
+            // Consume subtree
+            DeEvent::Start(e) => self.de.read_to_end(e.name()),
+            // Does not needed to deserialize using SimpleTypeDeserializer, because
+            // it returns `()` when `deserialize_unit()` is requested
+            DeEvent::Text(_) => Ok(()),
+            // SAFETY: the other events are filtered in `variant_seed()`
+            _ => unreachable!("Only `Start` or `Text` events are possible here"),
+        }
+    }
+
+    fn newtype_variant_seed<T>(self, seed: T) -> Result<T::Value, Self::Error>
+    where
+        T: DeserializeSeed<'de>,
+    {
+        if self.is_text {
+            match self.de.next()? {
+                DeEvent::Text(e) => seed.deserialize(SimpleTypeDeserializer::from_text_content(e)),
+                // SAFETY: the other events are filtered in `variant_seed()`
+                _ => unreachable!("Only `Text` events are possible here"),
+            }
+        } else {
+            seed.deserialize(&mut *self.de)
+        }
+    }
+
+    fn tuple_variant<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        if self.is_text {
+            match self.de.next()? {
+                DeEvent::Text(e) => {
+                    SimpleTypeDeserializer::from_text_content(e).deserialize_tuple(len, visitor)
+                }
+                // SAFETY: the other events are filtered in `variant_seed()`
+                _ => unreachable!("Only `Text` events are possible here"),
+            }
+        } else {
+            self.de.deserialize_tuple(len, visitor)
+        }
+    }
+
+    fn struct_variant<V>(
+        self,
+        fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        if self.is_text {
+            match self.de.next()? {
+                DeEvent::Text(e) => SimpleTypeDeserializer::from_text_content(e)
+                    .deserialize_struct("", fields, visitor),
+                // SAFETY: the other events are filtered in `variant_seed()`
+                _ => unreachable!("Only `Text` events are possible here"),
+            }
+        } else {
+            self.de.deserialize_struct("", fields, visitor)
         }
     }
 }
