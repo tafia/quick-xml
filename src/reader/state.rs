584406e714abeaf7d46e22c18a2dfe48b924cee0
Mingun <Alexander_Sergey@mail.ru>
2023-11-19 21:12:37 +0500

    Inline ReaderState::emit_bang with obvious dead code elimination (removed unused arms in match)

--- src/reader/state.rs
+++ src/reader/state.rs
@@ -83,86 +83,6 @@ pub(super) struct ReaderState {
 }
 
 impl ReaderState {
-    /// reads `BytesElement` starting with a `!`,
-    /// return `Comment`, `CData` or `DocType` event
-    pub fn emit_bang<'b>(&mut self, bang_type: BangType, buf: &'b [u8]) -> Result<Event<'b>> {
-        let uncased_starts_with = |string: &[u8], prefix: &[u8]| {
-            string.len() >= prefix.len() && string[..prefix.len()].eq_ignore_ascii_case(prefix)
-        };
-
-        let len = buf.len();
-        match bang_type {
-            BangType::Comment if buf.starts_with(b"!--") => {
-                debug_assert!(buf.ends_with(b"--"));
-                if self.config.check_comments {
-                    // search if '--' not in comments
-                    let mut haystack = &buf[3..len - 2];
-                    let mut off = 0;
-                    while let Some(p) = memchr::memchr(b'-', haystack) {
-                        off += p + 1;
-                        // if next byte after `-` is also `-`, return an error
-                        if buf[3 + off] == b'-' {
-                            // Explanation of the magic:
-                            //
-                            // - `self.offset`` just after `>`,
-                            // - `buf` contains `!-- con--tent --`
-                            // - `p` is counted from byte after `<!--`
-                            //
-                            // <!-- con--tent -->:
-                            //  ~~~~~~~~~~~~~~~~ : - buf
-                            //   : ===========   : - zone of search (possible values of `p`)
-                            //   : |---p         : - p is counted from | (| is 0)
-                            //   : :   :         ^ - self.offset
-                            //   ^ :   :           - self.offset - len
-                            //     ^   :           - self.offset - len + 2
-                            //         ^           - self.offset - len + 2 + p
-                            self.last_error_offset = self.offset - len + 2 + p;
-                            return Err(Error::IllFormed(IllFormedError::DoubleHyphenInComment));
-                        }
-                        // Continue search after single `-` (+1 to skip it)
-                        haystack = &haystack[p + 1..];
-                    }
-                }
-                Ok(Event::Comment(BytesText::wrap(
-                    // Cut of `!--` and `--` from start and end
-                    &buf[3..len - 2],
-                    self.decoder(),
-                )))
-            }
-            BangType::CData if uncased_starts_with(buf, b"![CDATA[") => {
-                debug_assert!(buf.ends_with(b"]]"));
-                Ok(Event::CData(BytesCData::wrap(
-                    // Cut of `![CDATA[` and `]]` from start and end
-                    &buf[8..len - 2],
-                    self.decoder(),
-                )))
-            }
-            BangType::DocType if uncased_starts_with(buf, b"!DOCTYPE") => {
-                match buf[8..].iter().position(|&b| !is_whitespace(b)) {
-                    Some(start) => Ok(Event::DocType(BytesText::wrap(
-                        // Cut of `!DOCTYPE` and any number of spaces from start
-                        &buf[8 + start..],
-                        self.decoder(),
-                    ))),
-                    None => {
-                        // Because we here, we at least read `<!DOCTYPE>` and offset after `>`.
-                        // We want report error at place where name is expected - this is just
-                        // before `>`
-                        self.last_error_offset = self.offset - 1;
-                        return Err(Error::IllFormed(IllFormedError::MissingDoctypeName));
-                    }
-                }
-            }
-            _ => {
-                // <!....>
-                //  ^^^^^ - `buf` does not contain `<` and `>`, but `self.offset` is after `>`.
-                // ^------- We report error at that position, so we need to subtract 2 and buf len
-                self.last_error_offset = self.offset - len - 2;
-                Err(bang_type.to_err())
-            }
-        }
-    }
-
     /// Wraps content of `buf` into the [`Event::End`] event. Does the check that
     /// end name matches the last opened start name if `self.config.check_end_names` is set.
     pub fn emit_end<'b>(&mut self, buf: &'b [u8]) -> Result<Event<'b>> {
@@ -417,7 +337,57 @@ impl ReaderState {
                 debug_assert!(content.starts_with(b"<!--"), "{:?}", Bytes(content));
                 debug_assert!(content.ends_with(b"-->"), "{:?}", Bytes(content));
 
-                self.emit_bang(BangType::Comment, &content[1..content.len() - 1])
+                let bang_type = BangType::Comment;
+                let buf = &content[1..content.len() - 1];
+                let uncased_starts_with = |string: &[u8], prefix: &[u8]| {
+                    string.len() >= prefix.len() && string[..prefix.len()].eq_ignore_ascii_case(prefix)
+                };
+
+                let len = buf.len();
+                match bang_type {
+                    BangType::Comment if buf.starts_with(b"!--") => {
+                        debug_assert!(buf.ends_with(b"--"));
+                        if self.config.check_comments {
+                            // search if '--' not in comments
+                            let mut haystack = &buf[3..len - 2];
+                            let mut off = 0;
+                            while let Some(p) = memchr::memchr(b'-', haystack) {
+                                off += p + 1;
+                                // if next byte after `-` is also `-`, return an error
+                                if buf[3 + off] == b'-' {
+                                    // Explanation of the magic:
+                                    //
+                                    // - `self.offset`` just after `>`,
+                                    // - `buf` contains `!-- con--tent --`
+                                    // - `p` is counted from byte after `<!--`
+                                    //
+                                    // <!-- con--tent -->:
+                                    //  ~~~~~~~~~~~~~~~~ : - buf
+                                    //   : ===========   : - zone of search (possible values of `p`)
+                                    //   : |---p         : - p is counted from | (| is 0)
+                                    //   : :   :         ^ - self.offset
+                                    //   ^ :   :           - self.offset - len
+                                    //     ^   :           - self.offset - len + 2
+                                    //         ^           - self.offset - len + 2 + p
+                                    self.last_error_offset = self.offset - len + 2 + p;
+                                    return Err(Error::IllFormed(IllFormedError::DoubleHyphenInComment));
+                                }
+                                haystack = &haystack[p + 1..];
+                            }
+                        }
+                        Ok(Event::Comment(BytesText::wrap(
+                            &buf[3..len - 2],
+                            self.decoder(),
+                        )))
+                    }
+                    _ => {
+                        // <!....>
+                        //  ^^^^^ - `buf` does not contain `<` and `>`, but `self.offset` is after `>`.
+                        // ^------- We report error at that position, so we need to subtract 2 and buf len
+                        self.last_error_offset = self.offset - len - 2;
+                        Err(bang_type.to_err())
+                    }
+                }
             }
             FeedResult::EmitDoctype(_) => {
                 debug_assert!(content.len() > 9, "{:?}", Bytes(content));
@@ -428,7 +398,37 @@ impl ReaderState {
                 );
                 debug_assert!(content.ends_with(b">"), "{:?}", Bytes(content));
 
-                self.emit_bang(BangType::DocType, &content[1..content.len() - 1])
+                let bang_type = BangType::DocType;
+                let buf = &content[1..content.len() - 1];
+                let uncased_starts_with = |string: &[u8], prefix: &[u8]| {
+                    string.len() >= prefix.len() && string[..prefix.len()].eq_ignore_ascii_case(prefix)
+                };
+
+                let len = buf.len();
+                match bang_type {
+                    BangType::DocType if uncased_starts_with(buf, b"!DOCTYPE") => {
+                        match buf[8..].iter().position(|&b| !is_whitespace(b)) {
+                            Some(start) => Ok(Event::DocType(BytesText::wrap(
+                                &buf[8 + start..],
+                                self.decoder(),
+                            ))),
+                            None => {
+                                // Because we here, we at least read `<!DOCTYPE>` and offset after `>`.
+                                // We want report error at place where name is expected - this is just
+                                // before `>`
+                                self.last_error_offset = self.offset - 1;
+                                return Err(Error::IllFormed(IllFormedError::MissingDoctypeName));
+                            }
+                        }
+                    }
+                    _ => {
+                        // <!....>
+                        //  ^^^^^ - `buf` does not contain `<` and `>`, but `self.offset` is after `>`.
+                        // ^------- We report error at that position, so we need to subtract 2 and buf len
+                        self.last_error_offset = self.offset - len - 2;
+                        Err(bang_type.to_err())
+                    }
+                }
             }
             FeedResult::EmitPI(_) => {
                 debug_assert!(content.starts_with(b"<?"), "{:?}", Bytes(content));
