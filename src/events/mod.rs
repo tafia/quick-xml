8cc053c0e5b5c74cb2a3613ae0324e26aa80043b
Arellano Ruiz, Eugenio Salvador <eugenio.arellanoruiz@dlr.de>
2024-03-28 13:58:02 +0100

    Fixed documentation string

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -251,13 +251,13 @@ impl<'a> BytesStart<'a> {
         bytes.push(b'"');
     }
 
-    /// Adds new line in attribute
+    /// Adds new line in existing element
     pub fn push_newline(&mut self) {
         let bytes = self.buf.to_mut();
         bytes.push(b'\n');
     }
 
-    /// Adds indent inside an existing attribute.
+    /// Adds indent inside an existing element.
     pub fn push_indent(&mut self, indent: &[u8]) {
         let bytes = self.buf.to_mut();
         bytes.extend_from_slice(indent);
