c3272e61d2a41b0f5f678bc3059eaf3ea7c7e2d3
Mingun <alexander_sergey@mail.ru>
2024-06-18 19:39:34 +0500

    Use direct comparison of events instead of using macro
    Although this changes some checks for Text, CData, and Comment events - previously
    they checked `Deref` implementation of corresponding events, but this can be considered,
    as not planned side effect. Explicit testing of this implementation was added in one of
    previous commits

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -2,94 +2,81 @@ use std::borrow::Cow;
 use std::str::from_utf8;
 
 use quick_xml::events::attributes::Attribute;
-use quick_xml::events::Event::*;
+use quick_xml::events::{BytesCData, BytesEnd, BytesStart, BytesText, Event::*};
 use quick_xml::name::QName;
 use quick_xml::reader::Reader;
 
 use pretty_assertions::assert_eq;
 
-macro_rules! next_eq_name {
-    ($r:expr, $t:tt, $bytes:expr) => {
-        match $r.read_event().unwrap() {
-            $t(ref e) if e.name().as_ref() == $bytes => (),
-            e => panic!(
-                "expecting {}({:?}), found {:?}",
-                stringify!($t),
-                from_utf8($bytes),
-                e
-            ),
-        }
-    };
-}
-
-macro_rules! next_eq_content {
-    ($r:expr, $t:tt, $bytes:expr) => {
-        match $r.read_event().unwrap() {
-            $t(ref e) if e.as_ref() == $bytes => (),
-            e => panic!(
-                "expecting {}({:?}), found {:?}",
-                stringify!($t),
-                from_utf8($bytes),
-                e
-            ),
-        }
-    };
-}
-
-macro_rules! next_eq {
-    ($r:expr, Start, $bytes:expr) => (next_eq_name!($r, Start, $bytes););
-    ($r:expr, End, $bytes:expr) => (next_eq_name!($r, End, $bytes););
-    ($r:expr, Empty, $bytes:expr) => (next_eq_name!($r, Empty, $bytes););
-    ($r:expr, Comment, $bytes:expr) => (next_eq_content!($r, Comment, $bytes););
-    ($r:expr, Text, $bytes:expr) => (next_eq_content!($r, Text, $bytes););
-    ($r:expr, CData, $bytes:expr) => (next_eq_content!($r, CData, $bytes););
-    ($r:expr, $t0:tt, $b0:expr, $($t:tt, $bytes:expr),*) => {
-        next_eq!($r, $t0, $b0);
-        next_eq!($r, $($t, $bytes),*);
-    };
-}
-
 #[test]
 fn test_start_end() {
     let mut r = Reader::from_str("<a></a>");
-    next_eq!(r, Start, b"a", End, b"a");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("a")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
 fn test_start_end_with_ws() {
     let mut r = Reader::from_str("<a></a >");
-    next_eq!(r, Start, b"a", End, b"a");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("a")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
 fn test_start_end_attr() {
     let mut r = Reader::from_str("<a b=\"test\"></a>");
-    next_eq!(r, Start, b"a", End, b"a");
+
+    assert_eq!(
+        r.read_event().unwrap(),
+        Start(BytesStart::from_content("a b=\"test\"", 1))
+    );
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
 fn test_empty_attr() {
     let mut r = Reader::from_str("<a b=\"test\" />");
-    next_eq!(r, Empty, b"a");
+
+    assert_eq!(
+        r.read_event().unwrap(),
+        Empty(BytesStart::from_content("a b=\"test\" ", 1))
+    );
 }
 
 #[test]
 fn test_start_end_comment() {
     let mut r = Reader::from_str("<b><a b=\"test\" c=\"test\"/> <a  /><!--t--></b>");
     r.config_mut().trim_text(true);
-    next_eq!(r, Start, b"b", Empty, b"a", Empty, b"a", Comment, b"t", End, b"b");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("b")));
+    assert_eq!(
+        r.read_event().unwrap(),
+        Empty(BytesStart::from_content("a b=\"test\" c=\"test\"", 1))
+    );
+    assert_eq!(
+        r.read_event().unwrap(),
+        Empty(BytesStart::from_content("a  ", 1))
+    );
+    assert_eq!(r.read_event().unwrap(), Comment(BytesText::new("t")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("b")));
 }
 
 #[test]
 fn test_start_txt_end() {
     let mut r = Reader::from_str("<a>test</a>");
-    next_eq!(r, Start, b"a", Text, b"test", End, b"a");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("a")));
+    assert_eq!(r.read_event().unwrap(), Text(BytesText::new("test")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
 fn test_comment() {
     let mut r = Reader::from_str("<!--test-->");
-    next_eq!(r, Comment, b"test");
+
+    assert_eq!(r.read_event().unwrap(), Comment(BytesText::new("test")));
 }
 
 #[test]
@@ -128,31 +115,47 @@ fn test_xml_decl() {
 #[test]
 fn test_cdata() {
     let mut r = Reader::from_str("<![CDATA[test]]>");
-    next_eq!(r, CData, b"test");
+
+    assert_eq!(r.read_event().unwrap(), CData(BytesCData::new("test")));
 }
 
 #[test]
 fn test_cdata_open_close() {
     let mut r = Reader::from_str("<![CDATA[test <> test]]>");
-    next_eq!(r, CData, b"test <> test");
+
+    assert_eq!(
+        r.read_event().unwrap(),
+        CData(BytesCData::new("test <> test"))
+    );
 }
 
 #[test]
 fn test_start_attr() {
     let mut r = Reader::from_str("<a b=\"c\">");
-    next_eq!(r, Start, b"a");
+
+    assert_eq!(
+        r.read_event().unwrap(),
+        Start(BytesStart::from_content("a b=\"c\"", 1))
+    );
 }
 
 #[test]
 fn test_nested() {
     let mut r = Reader::from_str("<a><b>test</b><c/></a>");
-    next_eq!(r, Start, b"a", Start, b"b", Text, b"test", End, b"b", Empty, b"c", End, b"a");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("a")));
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("b")));
+    assert_eq!(r.read_event().unwrap(), Text(BytesText::new("test")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("b")));
+    assert_eq!(r.read_event().unwrap(), Empty(BytesStart::new("c")));
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
 fn test_escaped_content() {
     let mut r = Reader::from_str("<a>&lt;test&gt;</a>");
-    next_eq!(r, Start, b"a");
+
+    assert_eq!(r.read_event().unwrap(), Start(BytesStart::new("a")));
     match r.read_event() {
         Ok(Text(e)) => {
             assert_eq!(
@@ -177,7 +180,7 @@ fn test_escaped_content() {
             e
         ),
     }
-    next_eq!(r, End, b"a");
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
@@ -204,7 +207,7 @@ fn test_closing_bracket_in_single_quote_attr() {
         }
         x => panic!("expected <a attr='>'>, got {:?}", x),
     }
-    next_eq!(r, End, b"a");
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
@@ -231,7 +234,7 @@ fn test_closing_bracket_in_double_quote_attr() {
         }
         x => panic!("expected <a attr='>'>, got {:?}", x),
     }
-    next_eq!(r, End, b"a");
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
@@ -258,7 +261,7 @@ fn test_closing_bracket_in_double_quote_mixed() {
         }
         x => panic!("expected <a attr='>'>, got {:?}", x),
     }
-    next_eq!(r, End, b"a");
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
 
 #[test]
@@ -285,5 +288,5 @@ fn test_closing_bracket_in_single_quote_mixed() {
         }
         x => panic!("expected <a attr='>'>, got {:?}", x),
     }
-    next_eq!(r, End, b"a");
+    assert_eq!(r.read_event().unwrap(), End(BytesEnd::new("a")));
 }
