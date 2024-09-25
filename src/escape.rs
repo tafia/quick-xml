1c2ddc526c64310155433c06c2342e152f90746b
yorkz1994 <york-z@outlook.com>
2024-09-25 21:51:17 +0800

    Update src/escape.rs
    Co-authored-by: Mingun <Alexander_Sergey@mail.ru>

--- src/escape.rs
+++ src/escape.rs
@@ -301,7 +301,7 @@ where
 
 /// Normalize the line end, replace \r or \r\n with \n.
 #[inline]
-fn normalize_line_end<'a>(input: impl Into<Cow<'a, str>>) -> Cow<'a, str> {
+fn normalize_line_end<'a>(input: Cow<'a, str>) -> Cow<'a, str> {
     let input = input.into();
     let bytes = input.as_bytes();
     let mut normalized = None;
