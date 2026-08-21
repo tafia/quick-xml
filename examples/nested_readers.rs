ef8cc84a0796c01a62eb7c0b73145e7cc63ca478
Daniel Alley <dalley@redhat.com>
2026-08-21 11:22:30 -0400

    Fix a broken assertion in nested_readers

--- examples/nested_readers.rs
+++ examples/nested_readers.rs
@@ -63,7 +63,7 @@ fn main() -> Result<(), quick_xml::Error> {
     assert_eq!(found_tables.len(), 2);
     // pretty print the table
     println!("{:#?}", found_tables);
-    assert_eq!(found_tables[0].index, 2);
+    assert_eq!(found_tables[0].index, 1);
     assert_eq!(found_tables[0].rows.len(), 2);
     assert_eq!(found_tables[0].rows[0].len(), 4);
     assert_eq!(found_tables[0].rows[1].len(), 4);
